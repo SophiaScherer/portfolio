6e99b2ea1f6711fb9b5a3f03714817715fae0584
SophiaScherer <Sophiasch@gmail.com>
2026-06-26 13:32:30 -0700

    Add Vercel Speed Insights to layout
    Injects `SpeedInsights` into the root layout so performance metrics are collected across the site.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "../styles/main.scss";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { getResumeDownload } from "../lib/content";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const epilogue = Epilogue({
   subsets: ["latin"],
@@ -68,6 +69,7 @@ export default async function RootLayout({
         {children}
 
         <Footer />
+      <SpeedInsights />
       </body>
     </html>
   );
